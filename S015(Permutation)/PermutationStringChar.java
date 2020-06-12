57a7b4abf3102e4657ce759fc05047f0c67005a8
BharkaviS <41267360+BharkaviS@users.noreply.github.com>
2020-06-12 16:07:17 +0530

    Create PermutationStringChar.java

---
+++ S015(Permutation)/PermutationStringChar.java
@@ -0,0 +1,47 @@
+/*
+
+The program must accept a string S as the input. The program must print all the permutations of the string S as the
+output.
+
+Example Input/Output 1:
+Input:
+abc
+Output:
+abc
+acb
+bac
+bca
+cba
+cab
+
+Example Input/Output 2:
+Input:
+rack
+Output:
+rack
+rakc
+rcak
+rcka
+rkca
+rkac
+arck
+arkc
+acrk
+ackr
+akcr
+akrc
+cark
+cakr
+crak
+crka
+ckra
+ckar
+kacr
+karc
+kcar
+kcra
+krca
+krac
+
+*/
+
